3486794b5fae3807e548f39668fb0e2b92e70d64
Aurasplash <aurasplash@gmail.com>
2020-08-20 10:49:56 -0600

    SMN Spirit Perp cost
    Changed Spirit cost to old 75 values now that they cast spells.

--- src/map/utils/petutils.cpp
+++ src/map/utils/petutils.cpp
@@ -1258,20 +1258,42 @@ namespace petutils
         int16 cost = 0;
         if (id >= 0 && id <= 7)
         {
-            if (level < 19)
-                cost = 1;
-            else if (level < 38)
+            if (level < 5)
                 cost = 2;
-            else if (level < 57)
+            else if (level < 9)
                 cost = 3;
-            else if (level < 75)
+            else if (level < 14)
                 cost = 4;
-            else if (level < 81)
+            else if (level < 18)
                 cost = 5;
-            else if (level < 91)
+            else if (level < 23)
                 cost = 6;
-            else
+            else if (level < 27)
                 cost = 7;
+            else if (level < 32)
+                cost = 8;
+            else if (level < 36)
+                cost = 9;
+            else if (level < 40)
+                cost = 10;
+            else if (level < 45)
+                cost = 11;
+            else if (level < 49)
+                cost = 12;
+            else if (level < 54)
+                cost = 13;
+            else if (level < 58)
+                cost = 14;
+            else if (level < 63)
+                cost = 15;
+            else if (level < 67)
+                cost = 16;
+            else if (level < 72)
+                cost = 17;
+            else if (level < 81)
+                cost = 18;
+            else
+                cost = 19;
         }
         else if (id == 8)
         {
